6f933326b2300678943d5f077b0afcdea1b7b17a
ByeongSu Kim <kimbsu3000@gmail.com>
2022-02-14 14:01:07 +0900

    Create MyData.kt

---
+++ Android/7/MyData.kt
@@ -0,0 +1,9 @@
+package com.example.recyclerviewtest
+
+data class MyData(val type: Int) {
+    companion object {
+        val TYPE_A: Int = 0
+        val TYPE_B: Int = 1
+        val TYPE_C: Int = 2
+    }
+}
